f0d1d286b850f9249a2ee1306a9c6ccb2591138f
Kyle <bossertkt@gmail.com>
2023-12-11 10:36:49 -0800

    yay

--- ghi/src/SpotifyContainer.js
+++ ghi/src/SpotifyContainer.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import SpotifyPlayer from './SpotifyPlayer';
 import SongSearch from './SongSearch';
 import useUserDetails from './useUserDetails';
-import { Box, Grid, Card, CardContent, Typography } from '@mui/material';
+import { Box, Grid, Card, CardContent, Typography, Container, useTheme } from '@mui/material';
 import NavBar from './NavBar';
 
 
@@ -11,8 +11,9 @@ const SpotifyContainer = () => {
     const [searchResults, setSearchResults] = useState([]);
     const userDetails = useUserDetails();
     const token = userDetails?.spotify_access_token;
-
+    const theme = useTheme();
     const [open, setOpen] = useState(true);
+
     const toggleDrawer = () => {
         setOpen(!open);
     };
@@ -26,41 +27,29 @@ const SpotifyContainer = () => {
     };
 
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'column'}}>
+        <Box sx={{ display: 'flex', pt: '64px' }}>
             <NavBar open={open} toggleDrawer={toggleDrawer} />
-            <SpotifyPlayer token={token} trackUri={trackUri} />
-            <SongSearch onSearchResult={handleSearchResult} />
-            <Grid container spacing={2} justifyContent="center">
-                {searchResults.map(track => (
-                    <Grid item xs={12} sm={6} md={4} key={track.id}>
-                        <Card onClick={() => handleSelectTrack(track.uri)} sx={{ cursor: 'pointer' }}>
-                            <CardContent>
-                                <img src={track.album.images[0].url} alt={track.name} style={{ width: '100%', height: 'auto' }} />
-                                <Typography variant="h6">{track.name}</Typography>
-                                <Typography variant="body2">{track.artists[0].name}</Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
+            <Container maxWidth="lg" sx={{ mt: 3, mb: 3, flexGrow: 1, transition: 'margin-left 0.3s', marginLeft: open ? `240px` : `${theme.spacing(9)}` }}>
+                <SongSearch onSearchResult={handleSearchResult} />
+                <Grid container spacing={2} justifyContent="center">
+                    {searchResults.map(track => (
+                        <Grid item xs={12} sm={6} md={4} key={track.id}>
+                            <Card onClick={() => handleSelectTrack(track.uri)} sx={{ cursor: 'pointer' }}>
+                                <CardContent>
+                                    <img src={track.album.images[0].url} alt={track.name} style={{ width: '100%', height: 'auto' }} />
+                                    <Typography variant="h6">{track.name}</Typography>
+                                    <Typography variant="body2">{track.artists[0].name}</Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+            <Box sx={{ position: 'fixed', botom: 0, left: 0, right: 0, zIndex: 1000}}>
+                <SpotifyPlayer token={token} trackUri={trackUri} />
+            </Box>
         </Box>
     )
 };
 
 export default SpotifyContainer;
-
-
-    // return (
-    //     <div>
-    //         <SongSearch onSearchResult={handleSearchResult} />
-    //         <div>
-    //             {searchResults.map(track => (
-    //                 <div key={track.id} onClick={() => handleSelectTrack(track.uri)}>
-    //                     <img src={track.album.images[0].url} alt={track.name} style={{ height: '64px', width: '64px' }} />
-    //                     {track.name} - {track.artists[0].name}
-    //                 </div>
-    //             ))}
-    //         </div>
-    //         <SpotifyPlayer token={token} trackUri={trackUri} />
-    //     </div>
-    // );
